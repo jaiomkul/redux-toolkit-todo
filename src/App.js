570633975709f5947a8051929d525cb5de2f9ae7
Govind123 <jaygupta2601@gmail.com>
2023-02-16 12:20:55 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import TodoData from "./Component/todo-data/TodoData";
+
+function App() {
+  return (
+    <div className="App">
+      <TodoData></TodoData>
+    </div>
+  );
+}
+
+export default App;
--- src/Component/redux/store.js
+++ src/Component/redux/store.js
@@ -1,11 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import todoReducer from "./todoSlice";
-import { createWrapper } from 'next-redux-wrapper';
 
 const store = configureStore({
   reducer: {
     todo: todoReducer,
   },
 });
-export const wrapper = createWrapper(store);
+
 export default store;
